3124b0a6ed7353311e4d8657a6270cf735437330
SIXAWE <107808660+SIXAWE@users.noreply.github.com>
2023-04-20 20:48:29 +0500

    Add files via upload

---
+++ components/TodoList.js
@@ -0,0 +1,113 @@
+import React from "react"
+import {StyleSheet,Text,View,TouchableOpacity,Modal} from 'react-native'
+import Colors from "../Colors";
+import TodoModal from "./TodoModal";
+
+
+
+
+export default  class TodoList extends React.Component{
+    state = {
+
+        showListVisible: false
+    }
+
+    toggleListModal(){
+        this.setState({showListVisible: !this.state.showListVisible})
+    }
+
+    render(){
+        const list = this.props.list
+        const completedCount = list.todos.filter(todo => todo.completed).length;
+        const remainingCount = list.todos.length - completedCount;
+        
+        return(
+            <View>
+                <Modal animationType="slide" visible={this.state.showListVisible} onRequestClose={() => this.toggleListModal()}>
+ 
+
+
+                    <TodoModal list={list} closeModal ={() => this.toggleListModal()}
+                     updateList={this.props.updateList}
+                      />
+                </Modal>
+                
+                <TouchableOpacity style={[styles.listContainer , {backgroundColor:list.color}]} 
+                onPress={() => this.toggleListModal()}
+                  
+                
+                >
+                    <Text style={styles.listTitle} numberOflines={1}> 
+                        {list.name}
+                    
+                    </Text>
+        
+                    <View>
+                        <View style=  {{alignItems: "center"}}>
+                            <Text style={styles.count}>{remainingCount}</Text>
+                            <Text syle={styles.subtitle}>Remaining</Text>     
+                            
+                        </View>
+        
+                        <View style=  {{alignItems: "center"}}>
+                            <Text style={styles.count}>{completedCount}</Text>
+                            <Text syle={styles.subtitle}>Completed</Text>     
+                            
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            
+            </View>
+            
+        );
+    
+
+
+    }
+
+
+
+    
+
+};
+
+const styles = StyleSheet.create({
+
+    listContainer:{
+            paddinVertical: 32,
+            paddingHorizontal:16,
+            borderRadius:6,
+            marginHorizontal: 12,
+            alignItems: "center",
+            width:200
+
+    },
+    listTitle:{
+        fontSize: 24,
+        fontWeight: "700",
+        color: Colors.white,
+        marginBottom: 50
+
+    },
+    count:{
+        fontSize:48,
+        fontWeight: "300",
+        color: Colors.white,
+    },
+    subtitle:{
+            fontSize:12,
+            fontWeight: "700",
+            color: Colors.white
+
+    }
+
+});
+
+
+
+
+
+
+
+
+
