d13a94ff570d8415ffa6672a931bd6a38d224eaf
SIXAWE <107808660+SIXAWE@users.noreply.github.com>
2023-04-20 20:49:49 +0500

    Delete Colors.js

--- Colors.js
+++
@@ -1,9 +0,0 @@
-export default  colors = {
-    black: "#2D3436",
-    gray: "#A4A4A4",
-    lightGray: "#CACACA",
-    blue: "#24A6D9",
-    lisghBlue: "#A7CBD9",
-    white:"#FFFFF",
-    red:"#D85963"
-};
